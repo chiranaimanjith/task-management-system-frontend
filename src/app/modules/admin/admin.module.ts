45cdbd1a232b85f1b2d3daecb1855096d6e1b0b2
chiranaimanjith <chiranaimangeth777@gmail.com>
2024-11-10 18:07:32 +0530

    push

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { PostTaskComponent } from './component/post-task/post-task.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    PostTaskComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    
   ]
 })
 export class AdminModule { }
---
+++ src/app/modules/admin/component/post-task/post-task.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AdminService } from '../../Service/admin.service';
+
+@Component({
+  selector: 'app-post-task',
+  templateUrl: './post-task.component.html',
+  styleUrls: ['./post-task.component.scss']
+})
+export class PostTaskComponent {
+ constructor(private adminService: AdminService){
+  this.getUsers();
+ }
+
+ getUsers(){
+  this.adminService.getUsers().subscribe((res)=>{
+    console.log(res);
+    
+  })
+ }
+}
