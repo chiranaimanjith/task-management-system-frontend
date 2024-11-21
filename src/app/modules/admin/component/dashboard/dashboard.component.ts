87fd904f2398b159847dad268fd23ee0b9b5f7a2
chiranaimanjith <chiranaimangeth777@gmail.com>
2024-11-21 14:13:19 +0530

    as

--- src/app/modules/admin/component/dashboard/dashboard.component.ts
+++ src/app/modules/admin/component/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../Service/admin.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+listOfTasks : any=[];
+
+constructor(private service: AdminService){
+  this.getTasks();
+}
+
+getTasks(){
+  this.service.getAllTask().subscribe((res)=>{
+    this.listOfTasks=res;
+  })
+}
 
 }
